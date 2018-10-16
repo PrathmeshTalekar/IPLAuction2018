460ad2ef992003c6233a14d647ce066f2d69dbd8
PrathmeshTalekar <42110550+PrathmeshTalekar@users.noreply.github.com>
2018-10-16 11:06:24 +0530

    sell half remaining

--- app/src/main/java/com/spit/iplauction2018/iplauction2018/LobbyActivity.java
+++ app/src/main/java/com/spit/iplauction2018/iplauction2018/LobbyActivity.java
@@ -1,6 +1,8 @@
 package com.spit.iplauction2018.iplauction2018;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -55,6 +57,7 @@ public class LobbyActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener mAuthStateListener;
     String path;
     private DatabaseReference mDatabaseReference, reference;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -142,10 +145,15 @@ public class LobbyActivity extends AppCompatActivity {
                         } else {
                             Toast.makeText(LobbyActivity.this, "Joining", Toast.LENGTH_SHORT).show();
                             reference = FirebaseDatabase.getInstance().getReference(path);
-                            reference.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(new User(display_name, "" + 9999999, "" + 0))
+                            reference.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(new User(display_name, "" + 4500000, "" + 0))
                                     .addOnSuccessListener(new OnSuccessListener<Void>() {
                                         @Override
                                         public void onSuccess(Void aVoid) {
+                                            sharedPreferences = getSharedPreferences("MY_GAME", Context.MODE_PRIVATE);
+                                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                                            editor.putString("lobby", path);
+                                            editor.apply();
+//                                            String  hello =sharedPreferences.getString("lobby",null);
                                             intent.putExtra("lobby_path", path);
                                             startActivity(intent);
                                             finish();
@@ -181,7 +189,7 @@ public class LobbyActivity extends AppCompatActivity {
             reference.child("pin").setValue(pin).addOnSuccessListener(new OnSuccessListener<Void>() {
                 @Override
                 public void onSuccess(Void aVoid) {
-                    reference.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(new User(display_name, "" + 9999999, "" + 0))
+                    reference.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(new User(display_name, "" + 4500000, "" + 0))
                             .addOnSuccessListener(new OnSuccessListener<Void>() {
                                 @Override
                                 public void onSuccess(Void aVoid) {
@@ -193,6 +201,10 @@ public class LobbyActivity extends AppCompatActivity {
                                             reference.child("players").setValue(dataSnapshot.getValue()).addOnSuccessListener(new OnSuccessListener<Void>() {
                                                 @Override
                                                 public void onSuccess(Void aVoid) {
+                                                    sharedPreferences = getSharedPreferences("MY_GAME", Context.MODE_PRIVATE);
+                                                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                                                    editor.putString("lobby", path);
+                                                    editor.apply();
                                                     intent.putExtra("lobby_path", path);
                                                     startActivity(intent);
                                                     finish();
--- app/src/main/java/com/spit/iplauction2018/iplauction2018/PlayerAdapter.java
+++ app/src/main/java/com/spit/iplauction2018/iplauction2018/PlayerAdapter.java
@@ -1,6 +1,7 @@
 package com.spit.iplauction2018.iplauction2018;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -11,23 +12,36 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
 import com.ramotion.foldingcell.FoldingCell;
 
 import java.util.ArrayList;
 
 public class PlayerAdapter extends ArrayAdapter<Player> {
 
-    DatabaseReference playerReference;
+    DatabaseReference playerReference, userReference;
+    SharedPreferences sharedPreferences;
+    String lobby_in;
+    User user;
+    int price;
+    int i = 0;
 
     public PlayerAdapter(Context TeamFragment, ArrayList<Player> player) {
         super(TeamFragment, 0, player);
+        sharedPreferences = TeamFragment.getSharedPreferences("MY_GAME", Context.MODE_PRIVATE);
+        lobby_in = sharedPreferences.getString("lobby", null);
     }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         Player currentPlayer = getItem(position);
         View listItemView = convertView;
         if (listItemView == null) {
@@ -60,10 +74,35 @@ public class PlayerAdapter extends ArrayAdapter<Player> {
         sell.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Player clickedPlayer = (Player)v.getTag();
-                Toast.makeText(getContext(), "Selling Player "+clickedPlayer.getName(), Toast.LENGTH_SHORT).show();
-                clickedPlayer.setSold(0);
-                //TODO: SELL THE PLAYER
+                if (i == 0) {
+                    Player clickedPlayer = (Player) v.getTag();
+                    clickedPlayer.setSold(0);
+                    playerReference = FirebaseDatabase.getInstance().getReference(lobby_in + FirebaseAuth.getInstance().getCurrentUser().getUid() + "/players/player" + (position + 1));
+                    playerReference.child("sold").setValue(0).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            userReference = FirebaseDatabase.getInstance().getReference(lobby_in + FirebaseAuth.getInstance().getCurrentUser().getUid());
+                            userReference.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    user.setDisplayName(dataSnapshot.child("displayName").getValue(String.class));
+                                    user.setCash(dataSnapshot.child("cash").getValue(String.class));
+                                    user.setPoints(dataSnapshot.child("points").getValue(String.class));
+                                    user.setNumberOfPlayers(dataSnapshot.child("numberOfPlayers").getValue(Integer.class));
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
+                        }
+                    });
+                    i = 1;
+                } else {
+                    Toast.makeText(getContext(), "Already 1 player sold", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
         return listItemView;
